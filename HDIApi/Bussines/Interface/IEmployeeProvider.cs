98237df736998d745763ba989c1591469d4dd837
Jona <zS19023589@estudiantes.uv.mx>
2023-12-21 22:04:50 -0600

    creacion de id random y registro de clientes y empleados

---
+++ HDIApi/Bussines/Interface/IEmployeeProvider.cs
@@ -0,0 +1,16 @@
+﻿using HDIApi.DTOs;
+using HDIApi.Models;
+
+namespace HDIApi.Bussines.Interface
+{
+    public interface IEmployeeProvider
+    {
+        public Task<int> SetNewEmployee(EmployeeDTO newEmployee);
+
+        public Task<(int,List<EmployeeDTO>)> GetEmployeeList();
+        public Task<(int,EmployeeDTO)> GetEmployeeById(string IdEmployee);
+        public Task<int> SetUpdateEmployee(EmployeeDTO updatedEmployee);
+
+    }
+}
+
---
+++ HDIApi/DTOs/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+namespace HDIApi.DTOs
+{
+    public class EmployeeDTO
+    {
+        public string? IdEmployee {get; set;} = "0";
+        public string NameEmployee {  get; set; }
+
+        public string LastnameEmployee { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Rol { get; set; }
+
+        public DateTime? RegistrationDate {get; set;} 
+    }
+}
