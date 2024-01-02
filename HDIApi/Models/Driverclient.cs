fbb08ed10737de763c651ac900e09bc15ecedc35
Jona <zS19023589@estudiantes.uv.mx>
2024-01-01 20:10:48 -0600

    cambio de tipo a campo age en bd

--- HDIApi/Models/Driverclient.cs
+++ HDIApi/Models/Driverclient.cs
@@ -17,7 +17,7 @@ public partial class Driverclient
 
     public string? Password { get; set; }
 
-    public int? Age { get; set; }
+    public DateOnly? Age { get; set; }
 
     public virtual ICollection<Accident> Accidents { get; } = new List<Accident>();
 
